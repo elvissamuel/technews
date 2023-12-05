79e0f2991ebd8085effe1c6a3facbed331a6f0ca
elvissamuel <77748602+elvissamuel@users.noreply.github.com>
2023-12-05 07:26:47 +0100

    Add Latest Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
-import CategoriesList from './components/CategoriesList'
+import CategoriesList from '../components/CategoriesList'
+import Post from '@/components/Post'
 
 export default function Home() {
+  const postData:Array<any> = []
   return (
     <>
     <CategoriesList />
-      <div>
-        Post
-      </div>
+      {postData && postData.length > 0 ? ( <Post />) : (<div>No Post to Display</div>)}
     </>
   )
 }
